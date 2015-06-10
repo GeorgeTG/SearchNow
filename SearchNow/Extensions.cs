53672561e5406ec3182c5c0e618a7db74792f4b2
GeorgeTG <george_gougoudis@hotmail.com>
2015-06-11 00:19:21 +0300

    Added InformationWindows and default engines.xml built in

---
+++ SearchNow/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Interop;
+using System.IO;
+using System.Windows;
+
+namespace SearchNow {
+    public static class Extensions {
+        public static async void DoAfter(this Action action, TimeSpan delay) {
+            await Task.Delay(delay);
+            action();
+        }
+
+        public static ImageSource ToImageSource(this Icon icon) {
+            using (MemoryStream iconStream = new MemoryStream()) { 
+                icon.Save(iconStream);
+                iconStream.Seek(0, SeekOrigin.Begin);
+                return (ImageSource)System.Windows.Media.Imaging.BitmapFrame.Create(iconStream);
+            }
+        }
+    }
+}
